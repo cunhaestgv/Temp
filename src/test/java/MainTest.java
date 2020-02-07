7f7537db6e14cf38eb91239eea1d6ec7d544d7ce
Carlos Cunha <cunha.cas@gmail.com>
2020-02-07 11:07:54 +0000

    init

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testHello(){
+        assertEquals("Olá","Olá");
+    }
+}
